d6918078f005a7ee857829de4b0a2f5815e5440d
vajrang@parvate.com <vajrang@parvate.com>
2021-06-11 16:29:55 -0400

    tweak assert (missing data) + examples for hvac

--- main.py
+++ main.py
@@ -64,18 +64,74 @@ gasusage: pd.DataFrame = influx.query(
     """,
 )['rtlamr']
 
+elecusage: pd.DataFrame = influx.query(
+    f"""
+        SELECT difference(last("consumption")) / 100 as "usage_kwh"
+        FROM "rtlamr"."autogen"."rtlamr"
+        WHERE "endpoint_id"='21212177' 
+        AND time >= '2021-01-01'
+        AND TIME < '{todaystr}'
+        GROUP BY time(24h) TZ('America/New_York')
+    """,
+)['rtlamr']
+
+hvacheat: pd.DataFrame = influx.query(
+    f"""
+        SELECT sum("auxHeat1") as "heat_secs"
+        FROM "ecobee"."autogen"."usage"
+        WHERE time >= '2021-01-01'
+        AND TIME < '{todaystr}'
+        GROUP BY time(24h) TZ('America/New_York')
+    """,
+)['usage']
+
+hvaccool: pd.DataFrame = influx.query(
+    f"""
+        SELECT sum("compCool1") as "cool_secs"
+        FROM "ecobee"."autogen"."usage"
+        WHERE time >= '2021-01-01'
+        AND TIME < '{todaystr}'
+        GROUP BY time(24h) TZ('America/New_York')
+    """,
+)['usage']
+
 df = degdays.merge(gasusage, how='left', left_index=True, right_index=True)
+df = df.merge(elecusage, how='left', left_index=True, right_index=True)
 df = df.merge(degrees, how='left', left_index=True, right_index=True)
+df = df.merge(hvacheat, how='left', left_index=True, right_index=True)
+df = df.merge(hvaccool, how='left', left_index=True, right_index=True)
 df.dropna(inplace=True)
 
-l = LinearRegression().fit(np.array(df['degrees']).reshape(-1, 1), df['usage_ccf'])
+df.loc[(df['heat_secs'] > 0) & (df['cool_secs'] > 0), 'mode'] = 'both'
+df.loc[(df['heat_secs'] > 0) & (df['cool_secs'] == 0), 'mode'] = 'heat'
+df.loc[(df['heat_secs'] == 0) & (df['cool_secs'] > 0), 'mode'] = 'cool'
+df.loc[(df['heat_secs'] == 0) & (df['cool_secs'] == 0), 'mode'] = 'off'
+
+heating_days = df[df['mode'] == 'heat']
+heating_off_days = df[df['mode'] != 'heat']
+
+l = LinearRegression().fit(np.array(heating_days['degrees']).reshape(-1, 1), heating_days['usage_ccf'])
 DAILY_CCF_USAGE_WITH_NO_HEATING = 0.376  #obtained from 2020 summer gas readings
 balance_point = (DAILY_CCF_USAGE_WITH_NO_HEATING - l.intercept_) / l.coef_[0]
-print(f'Balance point: {balance_point:.1f}° F')
 
+print(f'Balance point: {balance_point:.1f}° F')
 sns.set(rc={'figure.figsize': (12, 8)})
 fig, ax = plt.subplots()
 ax.set(xlim=(0, 70), ylim=(0, 7))
-sns.regplot(x=df['degrees'], y=df['usage_ccf'], truncate=False)
-fig.savefig('./balpoint.png')
+sns.regplot(
+    x=heating_days['degrees'],
+    y=heating_days['usage_ccf'],
+    truncate=False,
+    scatter_kws=dict(s=50, linewidth=0, color='#BB6600'),
+)
+sns.scatterplot(
+    x=heating_off_days['degrees'],
+    y=heating_off_days['usage_ccf'],
+    color='#888888',
+    s=50,
+    linewidth=0,
+)
+fig.savefig('./balpoint.png', bbox_inches='tight')
 plt.show()
+
+sns.scatterplot(data=df, x='degrees', y='usage_kwh', hue='mode', s=50)
--- outliers.py
+++ outliers.py
@@ -15,5 +15,5 @@ def check_outliers(influx: DataFrameClient):
     all_df['pct_change'] = all_df['temperature_F'].pct_change()
 
     # values determined as of 3/30/2021, adjust if necessary
-    assert all_df['diff'].abs().max() < 5.5
+    assert all_df['diff'].abs().max() < 13
     assert all_df['pct_change'].abs().max() < 0.35
